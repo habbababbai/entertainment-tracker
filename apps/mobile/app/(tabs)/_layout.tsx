d39f7288bb5f7fa460ab67bd24dd38955f60a64a
habbababbai <qba.7145@gmail.com>
2025-11-13 22:27:21 +0100

    remove header from tabbar

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -2,7 +2,11 @@ import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
     return (
-        <Tabs>
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
             <Tabs.Screen
                 name="home"
                 options={{
