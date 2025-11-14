6f941aa438be41800d74ff786bf40e0a46f6bbe7
habbababbai <qba.7145@gmail.com>
2025-11-14 14:48:17 +0100

    fix lint errors for env tests

--- apps/mobile/__tests__/lib/utils/env.test.ts
+++ apps/mobile/__tests__/lib/utils/env.test.ts
@@ -1,13 +1,19 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 describe("env utils", () => {
     const originalNodeEnv = process.env.NODE_ENV;
 
     afterEach(() => {
         jest.resetModules();
-        if (originalNodeEnv === undefined) {
-            delete process.env.NODE_ENV;
-        } else {
-            process.env.NODE_ENV = originalNodeEnv;
-        }
+        Object.defineProperty(process, "env", {
+            value: {
+                ...process.env,
+                ...(originalNodeEnv !== undefined && {
+                    NODE_ENV: originalNodeEnv,
+                }),
+            },
+            writable: true,
+            configurable: true,
+        });
     });
 
     it("returns true when jest is defined (always true in test environment)", () => {
@@ -17,7 +23,11 @@ describe("env utils", () => {
     });
 
     it("returns true when NODE_ENV is 'test'", () => {
-        process.env.NODE_ENV = "test";
+        Object.defineProperty(process, "env", {
+            value: { ...process.env, NODE_ENV: "test" },
+            writable: true,
+            configurable: true,
+        });
         jest.resetModules();
 
         const { isTestEnv } = require("../../../lib/utils/env");
@@ -25,7 +35,11 @@ describe("env utils", () => {
     });
 
     it("returns true when jest is defined even if NODE_ENV is not 'test'", () => {
-        process.env.NODE_ENV = "production";
+        Object.defineProperty(process, "env", {
+            value: { ...process.env, NODE_ENV: "production" },
+            writable: true,
+            configurable: true,
+        });
         jest.resetModules();
 
         const { isTestEnv } = require("../../../lib/utils/env");
@@ -33,7 +47,11 @@ describe("env utils", () => {
     });
 
     it("returns true when NODE_ENV is 'test' even if jest is not explicitly set", () => {
-        process.env.NODE_ENV = "test";
+        Object.defineProperty(process, "env", {
+            value: { ...process.env, NODE_ENV: "test" },
+            writable: true,
+            configurable: true,
+        });
         jest.resetModules();
 
         const { isTestEnv } = require("../../../lib/utils/env");
